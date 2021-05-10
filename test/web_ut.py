ee61bebebbb618ad12763e263619c38e8c0a69e6
yangbitmex <yangbitmex@163.com>
2021-05-10 11:48:16 +0800

    修改 window 10 环境变量的取值方法

--- test/web_ut.py
+++ test/web_ut.py
@@ -15,7 +15,7 @@ class ISelenium(unittest.TestCase):
     # 读入配置文件
     def get_config(self):
         config = configparser.ConfigParser()
-        config.read(os.path.join(os.environ['HOMEPATH'], 'iselenium.ini'))
+        config.read(os.path.join(os.environ['USERPROFILE'], 'iselenium.ini'))
         return config
 
     def tearDown(self):
